16ec229cf3b6a3d4632eb0763708464778fe91f5
suresh160390 <suresha003@gmail.com>
2024-08-13 20:09:13 +0530

    Latest

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import ReactDOM from 'react-dom/client';
+import ReactDOM  from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>
+// );
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
