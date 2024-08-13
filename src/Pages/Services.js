7f3c279d21c445aa8152867af58390375ac3f581
suresh160390 <suresha003@gmail.com>
2024-08-13 20:16:05 +0530

    Latest

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -2,12 +2,14 @@ import React from 'react'
 
 const Services = () => {
   return (
-    <div className='Service'>
-      <iframe title='TPH' style={{ width: '100%',height: '100%'}}
-        src='https://app.powerbi.com/reportEmbed?reportId=20f80794-5ebd-4180-bc20-566405220056&groupId=445dfe32-d62a-4538-a382-30a5f3ceaf7e&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUVBU1QyLUQtUFJJTUFSWS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d' 
-        frameboder="0" allowFullScreen="true" >
-      </iframe>
-    </div>
+    <>
+      <div className='Service'>
+        <iframe title='TPH' style={{ width: '100%',height: '100%'}}
+          src='https://app.powerbi.com/reportEmbed?reportId=20f80794-5ebd-4180-bc20-566405220056&groupId=445dfe32-d62a-4538-a382-30a5f3ceaf7e&w=2&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly9XQUJJLVVTLUVBU1QyLUQtUFJJTUFSWS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldCIsImVtYmVkRmVhdHVyZXMiOnsidXNhZ2VNZXRyaWNzVk5leHQiOnRydWV9fQ%3d%3d' 
+          frameboder="0" allowFullScreen="true" >
+        </iframe>
+      </div>
+    </>
   )
 }
 
